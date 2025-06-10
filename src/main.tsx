2670e70fb7d55ae9e2555e7a62173a16863ab26e
Vinothkumar Palanisamy <99629814+Vinothkumar14@users.noreply.github.com>
2025-06-10 10:50:19 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from './contexts/AuthContext.tsx';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
