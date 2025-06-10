94f63b947b535bc193d91f5182af98d66a0feb13
Vinothkumar Palanisamy <99629814+Vinothkumar14@users.noreply.github.com>
2025-06-10 10:46:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/cms_frontend/',
+  base: '/cms_frontend/', // 👈 IMPORTANT for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
